08fca883a048c9ccac07f9301b3dda451e6c1b3b
xiaofu2730 <86200651+xiaofu2730@users.noreply.github.com>
2023-06-05 13:08:40 +0000

    hi

---
+++ js/page_script.js
@@ -0,0 +1,18 @@
+document.writeln("	<script src=\'../../js/jquery.min.js\'></script>");
+document.writeln("	<script src=\'../../js/jquery-migrate-3.0.1.min.js\'></script>");
+document.writeln("	<script src=\'../../js/popper.min.js\'></script>");
+document.writeln("	<script src=\'../../js/bootstrap.min.js\'></script>");
+document.writeln("	<script src=\'../../js/jquery.easing.1.3.js\'></script>");
+document.writeln("	<script src=\'../../js/jquery.waypoints.min.js\'></script>");
+document.writeln("	<script src=\'../../js/jquery.stellar.min.js\'></script>");
+document.writeln("	<script src=\'../../js/owl.carousel.min.js\'></script>");
+document.writeln("	<script src=\'../../js/jquery.magnific-popup.min.js\'></script>");
+document.writeln("	<script src=\'../../js/aos.js\'></script>");
+document.writeln("	<script src=\'../../js/jquery.animateNumber.min.js\'></script>");
+document.writeln("	<script src=\'../../js/bootstrap-datepicker.js\'></script>");
+document.writeln("	<script src=\'../../js/jquery.timepicker.min.js\'></script>");
+document.writeln("	<script src=\'../../js/scrollax.min.js\'></script>");
+document.writeln("	<!--<script src=\'https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false\'></script>-->");
+document.writeln("	<script src=\'../../js/google-map.js\'></script>");
+document.writeln("	<script src=\'../../js/main.js\'></script>");
+document.writeln("	<script async src=\'//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js\'></script>");
