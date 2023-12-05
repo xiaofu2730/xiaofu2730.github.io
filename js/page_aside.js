d6cb9068e7c99e18148c7fca80319b0312e657e9
xiaofu2730 <2872016414@qq.com>
2023-12-05 16:19:42 +0800

    bug fix

--- js/page_aside.js
+++ js/page_aside.js
@@ -1,3 +1,11 @@
+/*
+ * @Description: 
+ * @Author: Chen Xiaofu
+ * @Date: 2023-06-06 15:12:14
+ * @LastEditTime: 2023-12-05 16:19:12
+ * @LastEditors: Chen XiaoFu
+ * @Contact me: 2020302191130@whu.edu.cn
+ */
 document.writeln("		<aside id=\'colorlib-aside\' role=\'complementary\' class=\'js-fullheight text-center\'>");
 document.writeln("			<h1 id=\'colorlib-logo\'><a href=\'../../index.html\'><span class=\'img\'");
 document.writeln("						style=\'background-image: url(../../images/profile_photo/comic.jpg);\'></span>XiaoFu Chen</a></h1>");
@@ -6,8 +14,8 @@ document.writeln("				<ul>");
 document.writeln("					<li><a href=\'../../index.html\'>Home</a></li>");
 document.writeln("					<li><a href=\'../../collection.html\'>Collection</a></li>");
 document.writeln("					<li><a href=\'../../about.html\'>About Me</a></li>");
-document.writeln("					<li class=\'colorlib-active\'><a href=\'../../services.html\'>Biography</a></li>");
-document.writeln("					<li><a href=\'../../blog.html\'>Blog</a></li>");
+document.writeln("					<li><a href=\'../../services.html\'>Biography</a></li>");
+document.writeln("					<li class=\'colorlib-active\'><a href=\'../../blog.html\'>Blog</a></li>");
 document.writeln("					<li><a href=\'../../contact.html\'>Contact</a></li>");
 document.writeln("				</ul>");
 document.writeln("			</nav>");
